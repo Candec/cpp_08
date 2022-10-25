75acc3b2d8a94cac323ff67766b60b5110267609
candec <omnius.candec@gmail.com>
2022-10-25 01:12:49 +0100

    created hpp initial definition

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,6 +1,28 @@
 #ifndef SPAN_HPP
 # define SPAN_HPP
 
+# include <algorithm>
+# include <list>
 
+class span
+{
+	public:
+	span();
+	span(unsigned int size);
+	span(const span &src);
+	span& operator=(const span& src);
+	~span();
+
+	unsigned int getSize();
+	void setSize();
+
+	void addNumber(long long int n);
+	unsigned long long int shortestSpan();
+	unsigned long long int longestSpan();
+
+	private:
+	unsigned int size;
+	std::list <long long int> lis;
+};
 
 #endif
